e8551b9e4355aa33170f958ee51fe8dd262d7b13
shreyuraju <shreyu.raju@gmail.com>
2022-07-16 19:07:36 +0530

    VTU MAD LAB

---
+++ program2_simplecalculator/src/main/java/com/example/simplecalculator/CalulatorMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalulatorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calulator_main);
+    }
+}
